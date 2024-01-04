5b8252beaa4945034b9cc7f1036c0e8671675f1f
gufronnakaaw <gufronnakaaw@gmail.com>
2024-01-04 20:30:01 +0700

    update: session provider

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,17 @@
-import '@/styles/globals.css'
+import '@/styles/globals.css';
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+import { ThemeProvider } from '@material-tailwind/react';
+import { SessionProvider } from 'next-auth/react';
+
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
+  return (
+    <SessionProvider session={session}>
+      <ThemeProvider>
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SessionProvider>
+  );
 }
