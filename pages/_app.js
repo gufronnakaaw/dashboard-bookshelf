3a7a7c8fbca510722622c86befcecc56e9a75e72
gufronnakaaw <gufronnakaaw@gmail.com>
2024-01-05 18:40:45 +0700

    update: window event

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,22 @@
 import '@/styles/globals.css';
 
 import { ThemeProvider } from '@material-tailwind/react';
-import { SessionProvider } from 'next-auth/react';
+import { SessionProvider, getSession, signOut } from 'next-auth/react';
+import { useEffect } from 'react';
 
 export default function App({
   Component,
   pageProps: { session, ...pageProps },
 }) {
+  useEffect(() => {
+    window.addEventListener('focus', async () => {
+      const session = await getSession();
+      if (!session) {
+        return signOut();
+      }
+    });
+  }, []);
+
   return (
     <SessionProvider session={session}>
       <ThemeProvider>
